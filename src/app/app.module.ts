69928224a0260ebbe2f94a0a9de16484c3386f33
unknown <formacio@PC-1062626.formacio.local>
2022-10-13 10:27:01 +0200

    Esquema del Proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { NavbarComponent } from './pages/shared/navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ArtistaComponent } from './pages/artista/artista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    NavbarComponent,
+    ArtistaComponent
   ],
   imports: [
     BrowserModule,

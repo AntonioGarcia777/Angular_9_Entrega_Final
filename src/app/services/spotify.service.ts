20ee855da690a33cabc68bf0621169e19dec205f
unknown <formacio@PC-1062626.formacio.local>
2022-10-13 13:16:41 +0200

    Configuración del Servicio

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,18 @@ import { Injectable } from '@angular/core';
 })
 export class SpotifyService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getNewReleases() {
+
+    const headers = new HttpHeaders ({
+      'Authorization': 'Bearer BQATq_xKs81Hkk0HvaSjCPbtIDOaq8n_X6VdDfmpXERkS0He7TVC9i-6QmDWsmMc8eQPujZ64vJAL51PCetI5SxyKWEn1IdinDMyt7I5EgKFr9rvuIc'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/browse/new-releases?limit=20`, {headers});
+
+  }
+
+
+
 }
